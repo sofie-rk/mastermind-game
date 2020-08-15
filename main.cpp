e3e382d564cce429cbe49d1d0ec19b0af91e1cf9
sofie-rk <61412527+sofie-rk@users.noreply.github.com>
2020-08-15 15:49:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,3 @@
-//
-// This is example code from Chapter 2.2 "The classic first program" of
-// "Programming -- Principles and Practice Using C++" by Bjarne Stroustrup
-// 
-// keep_window_open() added for TDT4102, excercise 0
-
-// This program outputs the message "Hello, World!" to the monitor
 
 
 #include "mastermindGame.h"
@@ -14,7 +7,6 @@
 
 //------------------------------------------------------------------------------'
 
-// C++ programs start by executing the function main
 int main()
 {
 	srand(static_cast<unsigned int>(time(nullptr)));
